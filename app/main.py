c894f322970230f372f3829f09789d561ba4bdd9
Muhammad Azeem Khan <muhammadazeemkhan192@gmail.com>
2026-07-13 13:35:53 +0500

    Configure database connection and environment settings

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(
     title="Job Application Tracker API",
     version="1.0.0"
 )
+
 
 @app.get("/")
 def home():
