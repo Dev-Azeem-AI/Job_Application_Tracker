c5b4ed846279fb8ffb908833d454905be1bea303
Muhammad Azeem Khan <muhammadazeemkhan192@gmail.com>
2026-07-14 14:23:49 +0500

    Implement password hashing with bcrypt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-
+from app.models.user import User
 from app.database import engine, Base
+from app.auth.hashing import hash_password
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,4 +15,25 @@ app = FastAPI(
 def home():
     return {
         "message": "Welcome to Job Application Tracker API"
+    }
+Base.metadata.create_all(bind=engine)
+@app.get("/health")
+def health():
+    return {
+        "status": "Database Connected",
+        "table": "users"
+    }
+    
+
+
+
+@app.get("/test-hash")
+def test_hash():
+    password = "123456"
+
+    hashed = hash_password(password)
+
+    return {
+        "original": password,
+        "hashed": hashed
     }
---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from datetime import datetime
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String, unique=True, nullable=False)
+
+    email = Column(String, unique=True, nullable=False)
+
+    hashed_password = Column(String, nullable=False)
+
+    is_active = Column(Boolean, default=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
